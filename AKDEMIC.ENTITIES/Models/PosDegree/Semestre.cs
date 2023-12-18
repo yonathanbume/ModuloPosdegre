23e30f86e0711c3d40df5694419da30fb2e915d1
Jonathan Bustinza Mendoza <jonathanbume@gmail.com>
2023-12-18 09:14:08 -0500

    campos corregidos y agregados en modelo Semestre

--- AKDEMIC.ENTITIES/Models/PosDegree/Semestre.cs
+++ AKDEMIC.ENTITIES/Models/PosDegree/Semestre.cs
@@ -9,9 +9,13 @@ namespace AKDEMIC.ENTITIES.Models.PosDegree
     public  class Semestre
     {
         public Guid id { get; set; }
-        public string name { get; set; }
-        public DateTime startaDate { get; set; }
-        public DateTime endaDate { get; set;}
-        
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set;}
+        public int CreditMin { get; set; }
+        public int CreditMax { get;set; }
+        public int NotaMin { get; set; }
+        public int NotaMax { get; set; }
+
     }
 }
