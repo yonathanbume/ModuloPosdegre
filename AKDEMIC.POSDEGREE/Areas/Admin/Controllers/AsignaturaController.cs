8180d715783a2fcfa0dae03ebbae55e487750b6b
Jonathan Bustinza Mendoza <jonathanbume@gmail.com>
2023-12-19 12:47:27 -0500

    se crea la estructura  y el diseño para asignaturas

---
+++ AKDEMIC.POSDEGREE/Areas/Admin/Controllers/AsignaturaController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using AKDEMIC.POSDEGREE.Controllers;
+using Microsoft.AspNetCore.Authorization;
+
+namespace AKDEMIC.POSDEGREE.Areas.Admin.Controllers
+{
+    public class AsignaturaController : BaseController
+    {
+       [Authorize]
+       [Area("Admin")]
+       [Route("admin/asignatura")]
+        // GET: GestionController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
